48da845e6c621ec8e646edb45aaf69821d377e57
hadoop <hadoop@Adams-MBP-2.attlocal.net>
2021-09-07 16:48:26 -0400

    Added basic stats and better formatting in main script.

--- src/main/scala/stocks/StockAnalysis.scala
+++ src/main/scala/stocks/StockAnalysis.scala
@@ -1,24 +1,62 @@
 package stocksproject
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.DataFrame
+
+object Helpers {
+  def prnt(strs:Seq[Any]) = {
+    print(strs.mkString(" | ") + "\n")
+  }
+}
 
 object StockAnalysis {
-	def main(args:Array[String]):Unit = {
+	import Helpers._
+
+  def main(args:Array[String]):Unit = {
 		val spark:SparkSession = SparkSession
   		.builder()
 	  	.appName("Project-3")
       .master("local")
 //		  .config("spark.master", "yarn-cluster")
-//  		.config("spark.broadcast.compress", "false")
-//	  	.config("spark.option", "value")
-		.getOrCreate()
+      .getOrCreate()
 
-		import spark.implicits._
+    // Hide "INFO: blah..." stuff
+    spark.sparkContext.setLogLevel("ERROR")
+    
+    import spark.implicits._
+  
+    val StockDF = createDF(spark)
+  		
+    StockDF.show(10, false)
+  
+    basicStats(spark, StockDF)
 
-		val df = spark.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://localhost:9000/stock-data/Google.csv")
+    spark.stop()
+  }
+  
+  /**
+    Creates the base DataFrame for analysis.
+  */
+  def createDF(spark:SparkSession) : DataFrame = {
+		spark.read.format("csv")
+      .option("inferSchema","true")
+      .option("header","true")
+      .load("hdfs://localhost:9000/stock-data/*.csv")
+      .withColumn("filename",input_file_name)
+      .withColumn("Company", reverse(split(col("filename"),"/")).getItem(0))
+  }
 
-		df.printSchema
-		
-		spark.stop()
-	}
+  /**
+    Basic stats.
+  */
+  def basicStats(spark:SparkSession, df:DataFrame) = {
+    // DataFrame size
+    prnt(Seq("DataFrame size",df.count()))
+
+    // DataFrame composition
+    df.groupBy("Company").count.show(10, false)
+
+    
+  }
 }
